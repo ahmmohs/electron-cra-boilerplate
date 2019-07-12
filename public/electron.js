cb1a8f85b5d782dde96117b4340a57b089e256ae
awsaf <awsafsikdar@gmail.com>
2019-07-11 23:53:16 -0400

    working dev environment

---
+++ public/electron.js
@@ -0,0 +1,51 @@
+const electron = require('electron');
+const { BrowserWindow, ipcMain } = electron;
+const isDev = require('electron-is-dev');
+const path = require('path');
+
+const app = electron.app;
+let mainWindow;
+
+/**
+ * createWindow - Creates the initial browser window for the app.
+ * @return {null}
+ */
+function createWindow() {
+
+    // create the browser window, with the specified settings/options
+    // learn more about BrowserWindow in official electron api
+    mainWindow = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        webPreferences: {
+            webSecurity: false,
+            nodeIntegration: true,
+            preload: __dirname + '/preload.js'
+        },
+        show: true
+    });
+
+    // set the view for the window, by either going to localhost:3000
+    // or if in production, grabbing the build file
+    mainWindow.loadURL(
+        isDev
+            ? 'http://localhost:3000'
+            : `file://${path.join(__dirname, '../build/index.html')}`
+    );
+    
+    // once the window is ready show it
+    mainWindow.on('ready-to-show', () => {
+        mainWindow.show();
+    });
+
+    // when renderer sends closeApp to to main, exit the process.
+    ipcMain.on('closeApp', e => {
+        console.log('Got called to close the app')
+        e.preventDefault();
+        process.exit();
+    })
+
+}
+
+// once electron app is ready, createWindow
+app.on('ready', createWindow);
